2baa9f2980ad0862938c859593e29aa27b48813c
menewol-home <wolfgang@menezes.at>
2015-11-05 23:16:40 +0100

    done

--- a3/a3.c
+++ a3/a3.c
@@ -1,45 +1,48 @@
 #include<stdio.h>
 
-double CalcVol(double piV, double radiusV, double heightV)
+#define PI (3.141592)
+
+double CalcVol(double radiusV, double heightV)
 {
 	//Volumen berechnet sich durch Grundflaeche * Hoehe = (pi*r*r)*h
-	return ((piV*radiusV*radiusV) * heightV);
+	return ((PI*radiusV*radiusV) * heightV);
 }
 
-double CalcMantel(double piM, double radiusM, double heightM)
+double CalcMantel(double radiusM, double heightM)
 {
 	//Mantelfläche berechnet sich durch Hoehe * Umfang = 2*pi*r*h
-	return 2 * piM * radiusM * heightM;
+	return 2 * PI * radiusM * heightM;
 }
 
-double CalcOberfl(double piO, double radiusO, double heightO)
+double CalcOberfl(double radiusO, double heightO)
 {
 	//Oberflaeche berechnet sich durch 2 * Grundflaeche + Mantelflaeche = 2*(pi*r*r) + 2*pi*r*h
-	return ((2*(piO*radiusO*radiusO)) + (2*piO*radiusO*heightO));
+	return ((2*(PI*radiusO*radiusO)) + (2*PI*radiusO*heightO));
 }
 
 int main(void)
 {
-	double pi = 3.141592, r = 2.5, h = 12.5; //Pi, Radius und Hoehe des Zylinders definieren
+	double r = 2.5, h = 12.5; // Radius und Hoehe des Zylinders definieren
 	double result; //Ergebnisse zwischenspeichern
 
 	//Ausgabe der Angaben zur Berechnung
 	printf("Berechnungen im Zylinder:\n\n");
 	printf("---------------------------------------------\n");
-	printf("Pi = %.2f\t r = %.2f cm\t h = %.2f cm\n",pi,r,h);
+	printf("Pi = %.2f\t r = %.2f cm\t h = %.2f cm\n",PI,r,h);
 	printf("---------------------------------------------\n");
 	
 	//Volumen berechnen, abspeichern & ausgeben
-	result = CalcVol(pi,r,h);
+	result = CalcVol(r,h);
 	printf("Das Volumen des Zylinders:\t  %.2f cm^3\n",result);
 
 	//Mantelflaeche berechnen, abspeichern & ausgeben
-	result = CalcMantel(pi,r,h);
+	result = CalcMantel(r,h);
 	printf("Der Mantel des Zylinders:\t  %.2f cm^2\n",result);
 
 	//Oberflaeche berechnen, abspeichern & ausgeben
-	result = CalcOberfl(pi,r,h);
+	result = CalcOberfl(r,h);
 	printf("Die Oberflaeche des Zylinders:\t  %.2f cm^2\n",result);
 
 	return 0;
 }
+
--- a4/a4.c
+++ a4/a4.c
@@ -2,65 +2,43 @@
 
 int main(void)
 {
-	char z1, z2, z3, output;
+	char z1 = 16, z2 = 23, z3 = 12, z4 = 2, z5 = 4, z6 = 3, output;
 
-	/*	16 AND 23
-	*	16: 0001 0000
-	*	23: 0001 0111	AND
-	*		---------
-	*		0001 0000 => 16
-	*/
-	z1 = 16;
-	z2 = 23;
+	/*	16 AND 23 */
 	output = z1 & z2;
-	printf("16 AND 23 = %d\n", output);
+	printf("%-40s|%4d|\n","16 AND 23 =", output);
 
-	/*	16 OR 23
-	*	16: 0001 0000
-	*	23: 0001 0111	OR
-	*		---------
-	*		0001 0111 => 23
-	*/
-	z1 = 16;
-	z2 = 23;
+	printf("----------------------------------------------\n");
+
+	/*	16 OR 23 */
 	output = z1 | z2;
-	printf("16 OR 23 = %d\n", output);
+	printf("%-40s|%4d|\n","16 OR 23 =",  output);
 
-	/*	23 LEFTSHIFT 4
-	*	23: 0001 0111
-	*	23 << 4: 0111 0000 => 112
-	*/
-	z1 = 23;
-	z2 = 4;
-	output = z1 << z2;
-	printf("23 LEFTSHIFT 4 = %d\n", output);
+	printf("----------------------------------------------\n");
 
-	/*	16 RIGHTSHIFT 2
-	*	16: 0001 0000
-	*	16 >> 2: 0000 0100 => 4
-	*/
-	z1 = 16;
-	z2 = 2;
-	output = z1 >> z2;
-	printf("16 RIGHTSHIFT 2 = %d\n", output);
+	/*	23 LEFTSHIFT 4 */
+	output = z2 << z5;
+	printf("%-40s|%4d|\n","23 << 4 =",  output);
+
+	printf("----------------------------------------------\n");
+
+	/*	16 RIGHTSHIFT 2 */
+	output = z1 >> z4;
+	printf("%-40s|%4d|\n","16 >> 2 =",  output);
+
+	printf("----------------------------------------------\n");
 	
-	/*	23 LEFTSHIFT (23 RIGHTSHIFT 3)
-	*
-	*/
-	z1 = 23;
-	z2 = 3;
-	output = (z1 << (z1 >> z2));
-	//printf
+	/*	23 LEFTSHIFT (23 RIGHTSHIFT 3) */
+	output = (z2 << (z2 >> z6));
+	printf("%-40s|%4d|\n","23 << (23 >> 3) =",  output);
 
-	/*	(23 RIGHTSHIFT 2) AND (16 OR (NOT 12))
-	*
-	*/
-	z1 = 23;
-	z2 = 16;
-	z3 = 12;
-	output = (23 >> 2) & (z2 | (~z3));
-	//printf
+	printf("----------------------------------------------\n");
 
-	getchar();
+	/*	(23 RIGHTSHIFT 2) AND (16 OR (NOT 12)) */
+	output = (z2 >> z4) & (z1 | (~z3));
+	printf("%-40s|%4d|\n","(23 >> 2) & (16 | (~12)) =",  output);
+
+	printf("----------------------------------------------\n");
+
 	return 0;
 }
